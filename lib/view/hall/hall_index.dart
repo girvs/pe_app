296178ec45b8160a0e61991dbe164ac15ab5b0b5
kicck <>
2020-04-16 22:07:06 +0800

    修改大厅

--- lib/view/hall/hall_index.dart
+++ lib/view/hall/hall_index.dart
@@ -30,27 +30,29 @@ class _HallIndexPageState extends State<HallIndexPage>
         SliverToBoxAdapter(
           child: _buildSwiper(),
         ),
-
         SliverPersistentHeader(
           pinned: true,
           delegate: StickyBarDelegate(
             minHeight: 50.0,
             maxHeight: 50.0,
             child: Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              height: 60,
-              child: _buildCatalog(),
-            ),
+                //padding: EdgeInsets.only(left: 10),
+                color: Theme.of(context).scaffoldBackgroundColor,
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: _buildCatalogList(),
+                )),
           ),
         ),
         SliverGrid(
           delegate:
               SliverChildBuilderDelegate(_buildListCardItem, childCount: 20),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-          ),
+              crossAxisCount: 2,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 1.1),
         ),
       ],
     );
@@ -84,17 +86,24 @@ class _HallIndexPageState extends State<HallIndexPage>
   List<Widget> _buildCatalogList() {
     return List<Widget>.generate(
       dataList.length,
-      (int index) {
-        return ChoiceChip(
+      (int index) => Container(
+        margin: EdgeInsets.only(left: 10),
+        child: ChoiceChip(
+          //padding: EdgeInsets.only(left: 20),
+          labelPadding: EdgeInsets.only(left: 10, right: 10),
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          elevation: 100,
+          //avatarBorder: const CircleBorder(side: BorderSide.(11, 11)),
           label: Text(dataList[index]),
           selected: _value == index,
           onSelected: (bool selected) {
             setState(() {
+              print(index);
               _value = selected ? index : null;
             });
           },
-        );
-      },
+        ),
+      ),
     ).toList();
   }
 
@@ -118,84 +127,69 @@ class _HallIndexPageState extends State<HallIndexPage>
 
   _buildSwiper() {
     return Container(
-      height: 250.0,
+      height: 160.0,
       child: Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return Image.network(
-              "http://img.zcool.cn/community/01ed9c55499c160000019ae993c9a4.jpg@2o.jpg",
-              fit: BoxFit.fill,
-            );
-          },
-          itemCount: 5,
-          scrollDirection: Axis.horizontal,
-          loop: true,
-          duration: 300,
-          autoplay: true,
-          onIndexChanged: (index) {
-            debugPrint("index:$index");
-          },
-          onTap: (index) {
-            debugPrint("点击了第:$index个");
-          },
-//                control:SwiperControl(),
-          pagination: SwiperPagination(
-              alignment: Alignment.bottomRight,
-              margin: EdgeInsets.only(bottom: 20.0, right: 20.0),
-              builder: SwiperPagination.fraction),
-//                autoplayDelay: 3000,
-          autoplayDisableOnInteraction: true),
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            _imageUrl[index],
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: _imageUrl.length,
+        scrollDirection: Axis.horizontal,
+        loop: true,
+        duration: 300,
+        autoplay: true,
+        onIndexChanged: (index) {
+          //debugPrint("index:$index");
+        },
+        onTap: (index) {
+          //debugPrint("点击了第:$index个");
+        },
+        pagination: SwiperPagination(
+          alignment: Alignment.bottomCenter,
+          margin: EdgeInsets.only(
+            bottom: 0.0,
+            right: 20.0,
+          ),
+          builder: SwiperPagination.dots,
+        ),
+        autoplayDisableOnInteraction: true,
+      ),
     );
   }
 
   Widget _buildListCardItem(BuildContext context, int index) {
     return Container(
       padding: EdgeInsets.all(0),
+      //height: 180,
       decoration: new BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(0.0)),
         border:
             new Border.all(width: 1, color: Color.fromARGB(255, 225, 225, 225)),
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Image.asset(
             index % 2 > 0 ? 'assets/girl/1.jpg' : 'assets/girl/2.jpg',
             fit: BoxFit.fill,
-            height: 120,
+            height: (MediaQuery.of(context).size.width / 2 - 5) / 16 * 9,
+            width: MediaQuery.of(context).size.width / 2 - 5,
           ),
-          SizedBox(
-            height: 2,
+          // SizedBox(
+          //   height: 2,
+          // ),
+
+          
+          ListTile(
+            isThreeLine: true,
+            //contentPadding: EdgeInsets.all(0),
+            title: Text("静静的等待", overflow: TextOverflow.ellipsis),
+            subtitle: Text("超级无级美少女战士！",
+                maxLines: 1, overflow: TextOverflow.ellipsis),
+            trailing: Icon(Icons.attach_money),
           ),
-          Text(
-            "  静静的等待",
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontSize: 14),
-          ),
-          Text(
-            "  超级无级美少女战士！",
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontSize: 11),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "人气：2000",
-                style: TextStyle(fontSize: 11),
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              Text(
-                "态度：极好",
-                style: TextStyle(fontSize: 11),
-              )
-            ],
-          )
         ],
       ),
     );
