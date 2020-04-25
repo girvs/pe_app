14b7d60e67b2e3dfbcf4413d4e69d608460a57dd
kicck <>
2020-04-25 17:53:03 +0800

    个性

--- lib/view/mine/video_setting.dart
+++ lib/view/mine/video_setting.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webrtc/webrtc.dart';
 
 class VideoSettingPage extends StatefulWidget {
   @override
@@ -7,96 +6,113 @@ class VideoSettingPage extends StatefulWidget {
 }
 
 class _VideoSettingPageState extends State<VideoSettingPage> {
-  MediaStream _localStream;
-  final _localRenderer = new RTCVideoRenderer();
-  bool _inCalling = false;
-
-  @override
-  initState() {
-    super.initState();
-    initRenderers();
-    _makeCall();
-  }
-
-  @override
-  deactivate() {
-    super.deactivate();
-    if (_inCalling) {
-      _hangUp();
-    }
-    _localRenderer.dispose();
-  }
-
-  initRenderers() async {
-    await _localRenderer.initialize();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  _makeCall() async {
-    final Map<String, dynamic> mediaConstraints = {
-      "audio": true,
-      "video": {
-        "mandatory": {
-          "minWidth":
-              '640', // Provide your own width, height and frame rate here
-          "minHeight": '480',
-          "minFrameRate": '30',
-        },
-        "facingMode": "user",
-        "optional": [],
-      }
-    };
-
-    try {
-      navigator.getUserMedia(mediaConstraints).then((stream) {
-        _localStream = stream;
-        _localRenderer.srcObject = _localStream;
-      });
-    } catch (e) {
-      print(e.toString());
-    }
-    if (!mounted) return;
-
-    setState(() {
-      _inCalling = true;
-    });
-  }
-
-  _hangUp() async {
-    try {
-      await _localStream.dispose();
-      _localRenderer.srcObject = null;
-    } catch (e) {
-      print(e.toString());
-    }
-    setState(() {
-      _inCalling = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    var isDark = Theme.of(context).brightness == Brightness.dark;
-    var selectColor = isDark ? Colors.white : Colors.yellow[900];
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height - 120,
-        child: RTCVideoView(_localRenderer),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
-        child: Container(
-          //color: isDark ? Theme.of(context).primaryColor : selectColor,
-          width: MediaQuery.of(context).size.width,
-          alignment: Alignment(0, 0),
-          height: 120,
-          child: Text(
-            "预计24小时完成到账，确认提现",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
-          ),
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_webrtc/webrtc.dart';
+
+// class VideoSettingPage extends StatefulWidget {
+//   @override
+//   _VideoSettingPageState createState() => _VideoSettingPageState();
+// }
+
+// class _VideoSettingPageState extends State<VideoSettingPage> {
+//   MediaStream _localStream;
+//   final _localRenderer = new RTCVideoRenderer();
+//   bool _inCalling = false;
+
+//   @override
+//   initState() {
+//     super.initState();
+//     initRenderers();
+//     _makeCall();
+//   }
+
+//   @override
+//   deactivate() {
+//     super.deactivate();
+//     if (_inCalling) {
+//       _hangUp();
+//     }
+//     _localRenderer.dispose();
+//   }
+
+//   initRenderers() async {
+//     await _localRenderer.initialize();
+//   }
+
+//   // Platform messages are asynchronous, so we initialize in an async method.
+//   _makeCall() async {
+//     final Map<String, dynamic> mediaConstraints = {
+//       "audio": true,
+//       "video": {
+//         "mandatory": {
+//           "minWidth":
+//               '640', // Provide your own width, height and frame rate here
+//           "minHeight": '480',
+//           "minFrameRate": '30',
+//         },
+//         "facingMode": "user",
+//         "optional": [],
+//       }
+//     };
+
+//     try {
+//       navigator.getUserMedia(mediaConstraints).then((stream) {
+//         _localStream = stream;
+//         _localRenderer.srcObject = _localStream;
+//       });
+//     } catch (e) {
+//       print(e.toString());
+//     }
+//     if (!mounted) return;
+
+//     setState(() {
+//       _inCalling = true;
+//     });
+//   }
+
+//   _hangUp() async {
+//     try {
+//       await _localStream.dispose();
+//       _localRenderer.srcObject = null;
+//     } catch (e) {
+//       print(e.toString());
+//     }
+//     setState(() {
+//       _inCalling = false;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var isDark = Theme.of(context).brightness == Brightness.dark;
+//     var selectColor = isDark ? Colors.white : Colors.yellow[900];
+//     return Scaffold(
+//       body: Container(
+//         width: MediaQuery.of(context).size.width,
+//         height: MediaQuery.of(context).size.height - 120,
+//         child: RTCVideoView(_localRenderer),
+//       ),
+//       bottomNavigationBar: BottomAppBar(
+//         color: Colors.transparent,
+//         child: Container(
+//           //color: isDark ? Theme.of(context).primaryColor : selectColor,
+//           width: MediaQuery.of(context).size.width,
+//           alignment: Alignment(0, 0),
+//           height: 120,
+//           child: Text(
+//             "预计24小时完成到账，确认提现",
+//             style: TextStyle(fontSize: 16, fontWeight: FontWeight.w200),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
